a741214ec209211c50b57d98247d4a4d16182c8e
Dtech2preas <lefa4082@gmail.com>
2026-03-15 21:56:24 +0200

    Delete extract_text.py

--- extract_text.py
+++
@@ -1,9 +0,0 @@
-import fitz
-
-doc = fitz.open("Mathematics P1 May-June 2024 Eng.pdf")
-text = ""
-for page in doc:
-    text += page.get_text()
-
-with open("pdf_text.txt", "w") as f:
-    f.write(text)
